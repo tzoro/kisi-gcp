a82f5e43d9a48f5db482e5abc41f233aa31cf66f
Tino <tino@klassapp.com>
2021-12-30 12:12:04 +0200

    Enqueueing of jobs added

--- app/jobs/fetch_adds_job.rb
+++ app/jobs/fetch_adds_job.rb
@@ -1,7 +1,67 @@
+require "google/cloud"
+require "google/cloud/pubsub"
+
+#Module here till writing code, easier to work, then separate in files
+
+module ActiveJob
+  module QueueAdapters
+    class GcpAdapter < ActiveJob::QueueAdapters::AsyncAdapter
+
+      keyfile = "gkey.json"
+      gcp_id  = "awesome-beaker-336608"
+
+      creds = Google::Cloud::PubSub::Credentials.new keyfile
+      @@pubsub = Google::Cloud::PubSub.new(
+        project_id: gcp_id,
+        credentials: creds
+      )
+
+      def initialize(**executor_options)
+        @scheduler = Scheduler.new(**executor_options)
+      end
+
+      def enqueue(job) #:nodoc:
+
+        job_name  = job.class.name
+        topic     = @@pubsub.topic job_name
+
+        if topic.nil?
+          topic = @@pubsub.create_topic job_name
+        end
+
+        sub_name      = job_name + "-subscription"
+        subscription  = topic.subscription sub_name
+
+        if subscription.nil?
+          topic.subscribe sub_name
+        end
+
+        topic.publish job.job_id
+
+        @scheduler.enqueue JobWrapper.new(job), queue_name: job.queue_name
+      end
+
+    end
+  end
+end
+
 class FetchAddsJob < ApplicationJob
   queue_as :default
+  self.queue_adapter = :gcp
 
   def perform(*args)
     # Do something later
   end
 end
+
+# begin
+#   topic.publish_async "This is a test message." do |result|
+#     raise "Failed to publish the message." unless result.succeeded?
+#     puts "Message published asynchronously."
+#   end
+
+#   # Stop the async_publisher to send all queued messages immediately.
+#   topic.async_publisher.stop.wait!
+# rescue StandardError => e
+#   puts "Received error while publishing: #{e.message}"
+# end
