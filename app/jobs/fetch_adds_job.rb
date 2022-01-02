1b9773b7bea039dcb51d4fa02c796670af29d29e
Tino <tino@klassapp.com>
2022-01-02 08:57:51 +0200

    Fallback attempts added

--- app/jobs/fetch_adds_job.rb
+++ app/jobs/fetch_adds_job.rb
@@ -50,6 +50,12 @@ class FetchAddsJob < ApplicationJob
   queue_as :default
   self.queue_adapter = :gcp
 
+  fallback_wait_time = 5.minutes
+  fallback_attempts  = 2
+
+  retry_on ActiveRecord::Deadlocked, wait: fallback_wait_time, attempts: fallback_attempts
+  retry_on Net::OpenTimeout, Timeout::Error, wait: fallback_wait_time, attempts: fallback_attempts
+
   def perform(*args)
     # Do something later
     puts "DOING JOOOOOB"
